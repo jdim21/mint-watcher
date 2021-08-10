c88282781667aefff800bd4c8bb1ccebb5474767
jdim21 <jdim21@gmail.com>
2021-08-09 22:30:49 -0400

    remaining endpoint

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { stringify } from 'querystring';
+import { pool } from './database';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async getHello(): Promise<string> {
+    return await pool.connect().then(async (client) => {
+      try {
+        const selectQuery = {
+          name: 'select-remaining',
+          text: `
+            select count(*) from public.mints where assigned = true
+            `,
+        }
+        var res = await pool.query(selectQuery).catch((e: any) => {
+          console.log("Error selecting mints query: " + e);
+          client.release();
+          return 'xxxxx';
+        });
+        const remaining = 17576 - parseInt(res.rows[0]["count"]);
+        client.release();
+        return remaining;
+      } catch (e) {
+        console.log("Exception selecting mints from DB: " + e);
+        client.release();
+        return 'xxxxx';
+      }
+    }).catch((e: any) => {
+      console.log("Error connecting to DB: " + e);
+      return 'xxxxx';
+    });
   }
 }
